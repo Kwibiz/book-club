1b4974cbb90380490137f26247f494f9223e7e59
Viktor Malovanyi <quibiz.ua@gmail.com>
2023-11-10 10:30:56 +0200

    added sigup, login, profile page

---
+++ bookclub/main/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('login/', views.LoginUserView.as_view(), name='login'),
+    path('logout/', views.LogoutUserView.as_view(), name='logout'),
+    path('signup/', views.sign_up, name='sign_up'),
+    path('profile/', views.profile, name='profile'),
+]
